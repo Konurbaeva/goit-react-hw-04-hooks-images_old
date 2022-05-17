99bf30eb9c22cd4be1a0d2e883cc8c42b46328fd
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-17 16:57:08 +0200

    adding react-loader-spinner

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,22 @@
+import { data } from "./model";
+
 function Loader() {
     return (
         <>
-            <p>TTT</p>
+            <div className="row">
+                {data.map((loader, index) => (
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 p-5">
+                        <div
+                            data-tip={loader.name}
+                            data-for="happyFace"
+                            key={loader.name + index}
+                            className="loaderBox"
+                        >
+                            <loader.Component {...loader.props} />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
---
+++ src/components/Loader/model.js
@@ -0,0 +1,15 @@
+import {
+    RevolvingDot,
+} from "react-loader-spinner";
+
+export const data = [
+    {
+        Component: RevolvingDot,
+        props: {
+            color: "#0ead69",
+            height: 100,
+            width: 110
+        },
+        name: "RevolvingDot"
+    }
+];

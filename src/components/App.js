5ea4e9b89b0ef4ee6792178f7ca095113e17f790
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-17 13:45:57 +0200

    display hits on the browser

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Searchbar from "./Searchbar/Searchbar";
 // FULL https://pixabay.com/api/?key=25748459-63f23aee85add1030efa422f3&q=cat&image_type=photo&orientation=horizontal&per_page=12
 axios.defaults.baseURL = "https://pixabay.com/api/"
 const API_KEY = "25748459-63f23aee85add1030efa422f3"
-const query = "?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12";
+const query = "cat";
 
 
 export class App extends Component {
@@ -17,7 +17,11 @@ export class App extends Component {
 
     async componentDidMount() {
         const response = await axios.get(`?q=${query}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
-        this.setState({ hits: response.hits });
+
+        //  console.log('results: ' + JSON.stringify(response));
+
+        console.log(' response.data.hits: ' + JSON.stringify(response.data.hits));
+        this.setState({ hits: response.data.hits });
     }
 
     async handleSubmit() {
@@ -25,11 +29,12 @@ export class App extends Component {
     }
 
     render() {
-        const { id, webformatURL, largeImageURL } = this.state;
+        // const { id, webformatURL, largeImageURL } = this.state;
         return (
             <div>
+                {/* <ImageGallery id={id} webformatURL={webformatURL} largeImageURL={largeImageURL} /> */}
+                <ImageGallery hits={this.state.hits} />
                 <Searchbar onSubmit={this.handleSubmit} />
-                <ImageGallery id={id} webformatURL={webformatURL} largeImageURL={largeImageURL} />
             </div>
         );
     }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,10 +1,26 @@
-function ImageGallery({ id, webformatURL, largeImageURL }) {
+// function ImageGallery({ id, webformatURL, largeImageURL }) {
+
+//     return (<ul className="gallery">
+//         {hits.map(hit => (
+//           <li>{hit.id}</li>
+//           <li>{hit.webformatURL}</li>
+//           <li>{hit.largeImageURL}</li>
+//         ))}
+//     </ul>);
+// }
+
+function ImageGallery({ hits }) {
 
     return (<ul className="gallery">
-        <li>
-
-        </li>
+        {hits.map(hit => (
+            <>
+                <li>{hit.id}</li>
+                <li>{hit.webformatURL}</li>
+                <li>{hit.largeImageURL}</li>
+            </>
+        ))}
     </ul>);
 }
+
 
 export default ImageGallery;

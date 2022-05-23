6eaaef845930d0dee5a5e3d2ccee3cce554f4499
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-23 19:14:43 +0200

    trying out

--- src/components/App.js
+++ src/components/App.js
@@ -15,12 +15,13 @@ export class App extends Component {
     };
 
     handleChange = e => {
-        this.setState({ query: e.currentTarget.value });
+        this.setState({ query: e.target.value });
     };
 
     handleSubmit = e => {
         e.preventDefault();
         this.onSubmit(this.state.query);
+
         // reset query?
         //  this.setState({ query: '' });
     };
@@ -29,11 +30,23 @@ export class App extends Component {
         this.setState({ isLoading: true });
 
         try {
-            getSearch('Nature')
-                .then(response => response.json())
-                .then(hits => this.setState({ hits: hits }))
+            // getSearch('Nature')
+            //     .then(response => response.json())
+            //     .then(hits => this.setState({ hits: hits }))
 
-            // this.setState({ hits });
+
+            //  const hits = getSearch(this.state.query)
+            // .then(response => response.json())
+            // .then(hits => this.setState({ hits }))
+            // .error(error => console.error(error))
+
+            const resp = getSearch(this.state.query)
+            const hits = resp.json()
+            this.setState({ hits })
+
+            console.log('GET SEARCH: ' + hits)
+            //  this.setState({ hits });
+
         } catch (error) {
             this.setState({ error });
         } finally {
@@ -51,10 +64,10 @@ export class App extends Component {
                 <Searchbar
                     onSubmit={this.handleSubmit}
                     value={query}
-                    // onChange={(e) => this.setState(e.target.value)}
                     onChange={this.handleChange}
-                //  onChange={(e) => this.setState({ query: e.currentTarget.value })}
 
+                // onChange={(e) => this.setState(e.target.value)}
+                //  onChange={(e) => this.setState({ query: e.currentTarget.value })}
                 // onChange={(e) => this.setState(e.currentTarget.value)}
 
                 />

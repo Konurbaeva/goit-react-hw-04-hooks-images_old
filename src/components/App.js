236482f02e65155194ec17ef38e0669d5c7b8d3d
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-27 17:29:25 +0200

    trying out

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,16 @@ import Searchbar from "./Searchbar/Searchbar";
 // import Modal from "./Modal/Modal";
 
 import { getSearch } from "services/api"
-import Container from "./Container";
+// import Container from "./Container";
 
 export class App extends Component {
     state = {
         hits: [],
         searchQuery: '',
-        showModal: true,
+        showModal: false,
         page: 1,
         isLoading: false,
+        errorMsg: '',
     };
 
     handleFormSubmit = queryFromSearchbar => {
@@ -29,15 +30,15 @@ export class App extends Component {
         console.log('searchQuery', searchQuery)
 
         if (prevState.searchQuery !== searchQuery) {
-            getSearch(this.state.searchQuery)
+            getSearch(searchQuery, page)
                 .then(hits => this.setState({ hits }))
                 .catch(error => console.log(error));
-            if (prevState.page !== page) {
-                this.loadMore();
-            }
         }
-
+        if (prevState.page !== page) {
+            this.loadMore();
+        }
     }
+
 
     handleModalDialog = () => {
         this.toggleModal()
@@ -59,25 +60,19 @@ export class App extends Component {
     render() {
         // const { showModal, isLoading } = this.state;
 
-        const { isLoading } = this.state;
+        const { isLoading, errorMsg } = this.state;
         return (
             <div>
-                <Container>
-                    {/* {showModal && (
-                        <Modal onClose={this.toggleModal}>
-                            <Searchbar onSubmit={this.handleFormSubmit} />
-                            <ImageGallery images={this.state.hits} onClick={this.toggleModal} /></Modal>
-                            
-                    )}  */}
-
+                <div className="main-section">
                     <Searchbar onSubmit={this.handleFormSubmit} />
                     <ImageGallery images={this.state.hits} onClick={this.toggleModal} />
+                    {errorMsg && <p className="errorMsg">{errorMsg}</p>}
                     <div className="load-more">
                         <button onClick={this.loadMore} className="btn-grad">
                             {isLoading ? 'Loading...' : 'Load More'}
                         </button>
                     </div>
-                </Container>
+                </div>
             </div>
         );
     }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -3,9 +3,6 @@ import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
 
 function ImageGallery({ images }) {
 
-    console.log('received images', images);
-    console.log('images.largeImageURL', images.largeImageURL);
-
     return (
         <ul>
             {images.map(({ webformatURL, tags }) => (

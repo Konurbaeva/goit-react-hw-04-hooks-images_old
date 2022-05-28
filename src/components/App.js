969e8ea737297ffe1f194eae6d012549fa9a03f2
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-28 15:35:55 +0200

    refactored

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,15 @@ import { Component } from "react";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import Searchbar from "./Searchbar/Searchbar";
 // import Loader from "./Loader/Loader";
-import Modal from "./Modal/Modal";
+// import Modal from "./Modal/Modal";
 
 import { getSearch } from "services/api"
-// import { Modal } from "./Modal";
 
 export class App extends Component {
     state = {
         hits: [],
         searchQuery: '',
         showModal: false,
-        page: 1,
-        isLoading: false,
-        errorMsg: '',
-        per_page: 10,
     };
 
     handleFormSubmit = queryFromSearchbar => {
@@ -26,42 +21,24 @@ export class App extends Component {
     componentDidUpdate(prevProps, prevState) {
         const prevPage = prevState.page;
         const nextPage = this.state.page;
+        const prevQuery = prevState.searchQuery;
+        const nextQuery = this.state.searchQuery;
 
-        if (prevPage !== nextPage) {
-            this.loadResults();
+        console.log('prevProps ', prevProps)
+        console.log('prevPage ', prevPage)
+        console.log('nextPage ', nextPage)
+        console.log('prevQuery ', prevQuery)
+        console.log('nextQuery ', nextQuery)
+
+        if (prevQuery !== nextQuery) {
+            setTimeout(() => {
+                getSearch(nextQuery)
+                    .then(hits => this.setState({ hits }))
+                    .catch(error => console.log(error));
+            }, 3000);
         }
     }
 
-    loadResults = () => {
-        const { page, per_page } = this.state;
-
-        this.setState({ isLoading: true });
-
-        getSearch(this.state.searchQuery, page, per_page)
-            .then((hits) => {
-                this.setState({ hits: hits, errorMsg: '' });
-            })
-            .catch((error) =>
-                this.setState({
-                    errorMsg: 'Error while loading data. Try again later.'
-                })
-            )
-            .finally(() => {
-                this.setState({ isLoading: false });
-            });
-    };
-
-    loadMore = () => {
-        this.setState((prevState) => ({
-            page: prevState.page + 1
-        }));
-    };
-
-
-    handleModalDialog = () => {
-        this.toggleModal()
-        console.log("cliked");
-    };
 
     toggleModal = () => {
         this.setState(({ showModal }) => ({
@@ -69,28 +46,21 @@ export class App extends Component {
         }));
     };
 
-
     render() {
-
-        const { showModal, searchQuery, hits, isLoading, errorMsg } = this.state;
+        const { hits } = this.state;
         return (
-            <div className="main-section">
+            <div>
+                {/* {showModal && (
+                    <Modal onClose={this.toggleModal}>
+                        <style>{'body { background-color: teal; }'}</style>
+                        <Searchbar onSubmit={this.handleFormSubmit} />
+                        <ImageGallery images={hits} />
+                    </Modal>
+                )} */}
 
-                {showModal && (
-                    <Modal
-                        largeImageURL={hits.largeImageURL}
-                        onClose={this.toggleModal}
-                        description={searchQuery}
-                    />
-                )}
+                <style>{'body { background-color: teal; }'}</style>
                 <Searchbar onSubmit={this.handleFormSubmit} />
-                <ImageGallery images={hits} onClick={this.toggleModal} />
-                {errorMsg && <p className="errorMsg">{errorMsg}</p>}
-                <div className="load-more">
-                    <button onClick={this.loadMore} className="btn-grad">
-                        {isLoading ? 'Loading...' : 'Load More'}
-                    </button>
-                </div>
+                <ImageGallery images={hits} />
             </div>
         );
     }

f4d0ca1b672faecfa0e81a49a00c30b6385a410f
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-29 20:25:42 +0200

    remove unused

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,17 +1,5 @@
 import styles from './ImageGalleryItem.module.css';
 
-// function ImageGalleryItem({ webImage, openModal, description }) {
-//     return (
-//         <li>
-//             <img
-//                 src={webImage}
-//                 alt={description}
-//                 onClick={openModal}
-//             />
-//         </li>
-//     );
-// }
-
 function ImageGalleryItem({ webImage, openModal, description }) {
     return (
         <li className={styles.ImageGalleryItem}>
